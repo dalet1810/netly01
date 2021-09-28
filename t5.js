6245520a4f867174314ef4193bd7472191c7e109
dalet1810 <50907189+dalet1810@users.noreply.github.com>
2021-09-28 02:33:31 -0400

    add a data line

--- t5.js
+++ t5.js
@@ -1,8 +1,9 @@
 {
-"t5":
+"rsp":
 [
 {"Make":"toyota", "Model":"celica", "Price":12000},
 {"Make":"illin", "Model":"susita", "Price":11},
-{"Make":"citroen", "Model":"2CV", "Price":3003}
+{"Make":"citroen", "Model":"2CV", "Price":3003},
+{"Make":"ford", "Model":"mondeo", "Price":13000}
 ]
 }
